c50fb0d01e1972b1a3208139c92b5d09c005a7ed
unknown <singla.ankur147@gmail.com>
2019-07-25 23:04:35 -0400

    Updated Deliverable2

---
+++ Deliverable2/Version2/SinhCalculator.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+
+public class SinhCalculator extends Validator
+{
+    public static void main(String args[])
+    {
+        System.out.println("This is a Sinh(x) calculator");
+        System.out.println("Please enter the value of x:");
+        System.out.println("Note: The value of x must lie between -700 to 700");
+        Scanner sc = new Scanner(System.in);
+        double x;
+        String s = sc.nextLine();
+        Validator v = new Validator();
+        Boolean b = v.myValidator(s);
+        if(b)
+        {
+            try
+            {
+                // checking valid integer using parseInt() method
+                x = Double.parseDouble(s);
+                //System.out.println(x + " is a valid number");
+                System.out.print(Comparison.my_comparison(x));
+            }
+            catch (NumberFormatException e)
+            {
+                System.out.print("Input is not a valid number");
+            }
+        }
+        else
+        {
+            System.out.print("Please run the program again and enter a valid input");
+        }
+    }
+}
+
---
+++ Deliverable2/Version2/Validator.java
@@ -0,0 +1,86 @@
+
+public class Validator extends MyFuncs
+{
+    public static Boolean isNotNullFun(String s)
+    {
+        int len = MyFuncs.strLen(s);
+        if (s == null || MyFuncs.strLen(s) == 0)
+        {
+            System.out.println("The input cannot be empty");
+            return false;
+        }
+        else
+        {
+            return true;
+        }
+    }
+    public static Boolean isNotAlphaFun(String s)
+    {
+        int len = MyFuncs.strLen(s);
+        for (int i = 0; i < len; i++)
+        {
+            char c = s.charAt(i);
+            if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
+            {
+                System.out.println("The input should be numeric only. It cannot contain any alphabet.");
+                return false;
+            }
+        }
+        return true;
+    }
+    public static Boolean doesNotContainSpaceFun(String s)
+    {
+        int len = MyFuncs.strLen(s);
+        int count = 0;
+        for (int i = 0; i < len; i++)
+        {
+            char c = s.charAt(i);
+            if (c == ' ')
+            {
+                System.out.println("The input contains spaces");
+                return false;
+            }
+        }
+        return true;
+    }
+    public static Boolean doesNotContainSpecialCharFun(String s)
+    {
+        int len = MyFuncs.strLen(s);
+        String specialCharacters="!#$%&'()*+,/:;<=>?@[]^_`\\{|}";
+        for (int i = 0; i < MyFuncs.strLen(specialCharacters); i++)
+        {
+            for (int j  = 0; j < len; j++)
+            {
+                if (s.charAt(j) == specialCharacters.charAt(i))
+                {
+                    System.out.println("The input contains special character " + s.charAt(j));
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+    public static Boolean myValidator(String s)
+    {
+        if (!(Validator.isNotAlphaFun(s)))
+        {
+            return false;
+        }
+        else if (!(Validator.doesNotContainSpaceFun(s)))
+        {
+            return false;
+        }
+        else if (!(Validator.isNotNullFun(s)))
+        {
+            return false;
+        }
+        else if (!(Validator.doesNotContainSpecialCharFun(s)))
+        {
+            return false;
+        }
+        else
+        {
+            return true;
+        }
+    }
+}
